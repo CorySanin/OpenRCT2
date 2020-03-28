c80fb45b4fc99c6e36f9ece41bc4abb9e63934bf
Cory Sanin <corysanin@outlook.com>
2020-03-28 01:24:12 -0500

    default cheats, disable 'no entry' toggle

--- src/openrct2/actions/BannerSetStyleAction.hpp
+++ src/openrct2/actions/BannerSetStyleAction.hpp
@@ -143,6 +143,11 @@ public:
                     return MakeResult(GA_ERROR::UNKNOWN, STR_NONE);
                 }
 
+                if(!network_player_is_admin(network_get_player_index(GetPlayer().id))){
+                    log_error("No 'no entry' signs");
+                    return MakeResult(GA_ERROR::INVALID_PARAMETERS, STR_NONE);
+                }
+
                 banner->flags &= ~BANNER_FLAG_NO_ENTRY;
                 banner->flags |= (_parameter != 0) ? BANNER_FLAG_NO_ENTRY : 0;
                 uint8_t allowedEdges = 0xF;
