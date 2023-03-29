7874c2db4d16a1e60a48dfa2751d79c259ea0b18
Cory Sanin <corysanin@outlook.com>
2023-03-28 19:48:40 -0500

    Include embedded objects... again
    see 644922b2

--- src/openrct2/command_line/PrepCommand.cpp
+++ src/openrct2/command_line/PrepCommand.cpp
@@ -252,14 +252,12 @@ exitcode_t CommandLine::HandleCommandPrep(CommandLineArgEnumerator* enumerator)
 
     try
     {
-        auto exporter = std::make_unique<ParkFileExporter>();
-
         // HACK remove the main window so it saves the park with the
         //      correct initial view
         //      taken from ConvertCommand.cpp
         WindowCloseByClass(WindowClass::MainWindow);
 
-        exporter->Export(destinationPath);
+        SaveGameWithName(destinationPath);
     }
     catch (const std::exception& ex)
     {
